c9f148c5f1e5598bf846a5b893ced551d1d95fb6
Brandon Keefer <happysolucki@outlook.com>
2021-08-31 13:24:41 -0500

    added comments

--- inc1.js
+++ inc1.js
@@ -1,10 +1,16 @@
+// select elements in dom
 let header = document.querySelector("h2");
 let buttons = document.querySelectorAll("button");
 
+// make function for changing body bg color
 const setBodyBGColor = (evt) => {
+  // capture text that shows on the button
   let color = evt.target.textContent;
+  // change header text to text from button
   header.textContent = color;
+  // change
   document.body.style.backgroundColor = color.toLowerCase();
 };
 
-buttons.forEach((element) => element.addEventListener("click", setBodyBGColor));
+// add event listener
+buttons.forEach((element) => element.addEventListener("mouseover", setBodyBGColor));
--- inc2.js
+++ inc2.js
@@ -1,3 +1,4 @@
+// make colors array
 const colors = [
   "blue",
   "red",
@@ -8,11 +9,19 @@ const colors = [
   "black",
   "green",
 ];
+
+// select button from dom
 const button = document.querySelector("button");
+
+// initialize starting index
 let idx = 0;
 
+// add event listener on button
 button.addEventListener("click", () => {
+  // if idx equals length of colors array, reset idx to zero
   if (idx === colors.length) idx = 0;
+  // set bg color of body to current idx of colors array
   document.body.style.backgroundColor = colors[idx];
+  // increment idx by one
   idx++;
 });
